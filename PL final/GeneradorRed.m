4b0d077743967684d29076b88847713fcf109690
edugh02 <119579748+edugh02@users.noreply.github.com>
2024-01-11 11:30:11 +0100

    Red neuronal funcional
    Solo queda comprobar que te funciona en tu ordenador

--- PL final/GeneradorRed.m
+++ PL final/GeneradorRed.m
@@ -1,20 +1,14 @@
-% training_data = [];
-% for i =1:5
-%     aux = load("datos_entrenamiento"+i).training_data;
-%     training_data = vertcat(training_data,aux);
-% end
-
 training_data = load("datos_entrenamiento6").training_data;
 
 %Generar los inputs y los outputs
-inputs = training_data(:,[6,8,9,11]);
+inputs = training_data(:,[6,7,8,9,11]);
 outputs = training_data(:,[18,19]);
 inputs(isinf(inputs)) = 5.0;
 inputs = double(inputs');
 outputs = double(outputs');
 
 %Generación de la red
-net = feedforwardnet([10,10,5]);
+net = feedforwardnet([10,3,3]);
 net = configure(net,inputs,outputs);
 net = train(net,inputs,outputs);
 
